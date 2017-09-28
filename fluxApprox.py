3ba73332a913257dd9625da247358c95bb63949c
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-09-28 16:33:53 +0100

    fix bug (dont do x=y=something in python as x will always = y. stupid)

--- fluxApprox.py
+++ fluxApprox.py
@@ -27,26 +27,14 @@ def fluxSplitting(q, simulation):
     """
     fluxX = simulation.model.fluxX(q, simulation)
     fluxY = simulation.model.fluxY(q, simulation)
-    netFlux = netFluxX = netFluxY = np.zeros_like(fluxX)
+    netFlux = np.zeros_like(fluxX)
+    netFluxX = np.zeros_like(fluxX)
+    netFluxY = np.zeros_like(fluxX)
     
-    for var in range(q.shape[0]):
-        for i in range(1, q.shape[1]-1):
-            for j in range(q.shape[2]):
-                netFluxX[var, i, j] = (fluxX[var, i+1, j] - fluxX[var, i, j]) / simulation.deltaX
-    
-    for var in range(q.shape[0]):
-        for i in range(q.shape[1]):
-            for j in range(1, q.shape[2]-1):
-                netFluxY[var, i, j] = (fluxY[var, i, j+1] - fluxY[var, i, j]) / simulation.deltaY
-    
-    
-#    netFluxX[:, 1:-1, :] = (fluxX[:, 2:, :] - fluxX[:, 1:-1, :]) / simulation.deltaX
-#    netFluxY[:, :, 1:-1] = (fluxY[:, :, 2:] - fluxY[:, :, 1:-1]) / simulation.deltaY    
+    netFluxX[:, 1:-1, :] = (fluxX[:, 2:, :] - fluxX[:, 1:-1, :]) / simulation.deltaX
+    netFluxY[:, :, 1:-1] = (fluxY[:, :, 2:] - fluxY[:, :, 1:-1]) / simulation.deltaY  
     netFlux = netFluxX + netFluxY
 
-    print("Xflux:")
-    print((fluxX[:, 2:, :] - fluxX[:, 1:-1, :])[1, 51:57, 3:7] / simulation.deltaX)
-    print(netFluxX[1, 50:58 ,3:7])
 
     return netFlux
 
