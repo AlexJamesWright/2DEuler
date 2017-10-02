475dbe05247908705adb84510d308881ceb63e17
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-10-02 13:41:16 +0100

    Included periodic boundaries

--- Modules/boundaryConditions.py
+++ Modules/boundaryConditions.py
@@ -42,3 +42,36 @@ def outflow(q, cells):
         
     return q
 
+
+def periodic(q, cells):
+    """
+    Imposing flows that exit the system and re-enter in the other side.
+    What flows out of q_N enters at q_0 and viceversa.
+    Parameters
+    ----------
+    q: array of float
+        (Nvars, number_of_cells) The value of the convserved variables
+        at the centre of each corresponding cell
+    cells: object
+        A cell object, simulation.cells, containing the information about
+        the system's grid construction
+    Returns
+    -------
+    q: array of float
+        (Nvars, number_of_cells) The value of the convserved variables
+        at the centre of each corresponding cell
+    """
+    N = cells.Nghosts
+
+    for i in range(N):
+        for j in range(N, cells.ny + N):
+            q[:, i, j] = q[:, -2*N+i, j]
+            q[:, -N+i, j] = q[:, N+i, j]
+            
+    for i in range(N, cells.nx + N):
+        for j in range(N):
+            q[:, i, j] = q[:, i, -2*N+j]
+            q[:, i, -N+j] = q[:, i, N+j]
+
+    return q    
+
