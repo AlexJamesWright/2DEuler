210067899161e4067f518f70c824388c82d4da1d
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-10-29 16:08:59 +0000

    Playing with two fluid model: basically wanted to make sure I committed this weekend, gotta keep that grid looking full

--- Modules/play.py
+++ Modules/play.py
@@ -12,7 +12,7 @@ from model import TwoFluidEMHD
 from timeEv import eulerSplitRK3
 from boundaryConditions import periodic
 from fluxApprox import fluxSplitting
-from sourceTerms import sources, divCleaning
+from sourceTerms import sources, twoFluidDivClean
 import numpy as np
 import time
 
@@ -21,9 +21,9 @@ if __name__ == '__main__':
     timeStart = time.time()
 
     # Set up problem
-    grid = cells(10, 10, 0, 1, 0, 1)
+    grid = cells(30, 30, 0, 1, 0, 1)
     model = TwoFluidEMHD(grid=grid)
-    source = sources(divCleaning)
+    source = sources(twoFluidDivClean)
     # Set initial state
     initFuncObj = initialFunc(grid, model)
     initFunc = initFuncObj.OTVortexTwoFluid
@@ -32,17 +32,18 @@ if __name__ == '__main__':
     # Set up simulation
     sim = simulation(initFuncObj, initFunc, grid,
                      model, eulerSplitRK3, periodic, fluxSplitting, source=source,
-                     cfl=0.5)
+                     cfl=0.2)
 
     print("Running simulation")
-#    sim.runSim(0.8)
-#    sim.plotPrimHeatmaps() 
+    sim.runSim(0.1)
+    sim.plotPrimHeatmaps() 
 
 
-    z1, z2 = sim.model.getPrimitiveVars(sim.q, sim)
+
 
 
     timeTotal = time.time() - timeStart
     print("Run time: {:.0f}m {:.1f}s".format(timeTotal // 60, timeTotal%60 ))
+
 
 
