3930ce1286a0e6823e4be45db511e3ea2b764bf3
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-11-01 12:44:14 +0000

    IMEX scheme SSP2

--- Modules/timeEv.py
+++ Modules/timeEv.py
@@ -12,14 +12,14 @@ def euler(simulation, q):
     """
     Parameters
     ----------
-    simulation: class
+    simulation : object
         The class containing all the data regarding the simulation
-    q: numpy array of floats
+    q : numpy array of floats
         The value of the field at the centre of each corresponding cell
 
     Returns
     -------
-    q: numpy array of floats
+    q : numpy array of floats
         The new values of the field according to the flux given by
         simulation.F using the Euler approximation
         First order accurate in time
@@ -32,14 +32,14 @@ def RK2(simulation, q):
     """
     Parameters
     ----------
-    simulation: class
+    simulation : object
         The class containing all the data regarding the simulation
-    q: numpy array of floats
+    q : array of floats
         The value of the field at the centre of each corresponding cell
 
     Returns
     -------
-    q: numpy array of floats
+    q : array of floats
         The new values of the field according to the flux given by
         simulation.F using the Runge-Kutta 2-step approximation
         Second order accurate in time
@@ -55,14 +55,14 @@ def RK3(simulation, q):
     """
     Parameters
     ----------
-    simulation: class
+    simulation : object
         The class containing all the data regarding the simulation
-    q: numpy array of floats
+    q : array of floats
         The value of the field at the centre of each corresponding cell
 
     Returns
     -------
-    q: numpy array of floats
+    q : array of floats
         The new values of the field according to the flux given by
         simulation.F using the Runge-Kutta 3-step approximation
         Third order accurate in time
@@ -85,14 +85,14 @@ def eulerSplitRK3(simulation, q):
 
     Parameters
     ----------
-    simulation: class
+    simulation : object
         The class containing all the data regarding the simulation
-    q: numpy array of floats
+    q : array of floats
         The value of the field at the centre of each corresponding cell
 
     Returns
     -------
-    q: numpy array of floats
+    q : array of floats
         The new values of the field according to the flux given by
         simulation.F using the Runge-Kutta 3-step approximation and the
         forward Euler step
@@ -110,14 +110,14 @@ def eulerSplitRK2(simulation, q):
 
     Parameters
     ----------
-    simulation: class
+    simulation : object
         The class containing all the data regarding the simulation
-    q: numpy array of floats
+    q : array of floats
         The value of the field at the centre of each corresponding cell
 
     Returns
     -------
-    q: numpy array of floats
+    q : array of floats
         The new values of the field according to the flux given by
         simulation.F using the Runge-Kutta 3-step approximation and the
         forward Euler step
@@ -128,3 +128,85 @@ def eulerSplitRK2(simulation, q):
     qstar = RK2(simulation, q)
     primstar, auxstar, alphastar = simulation.model.getPrimitiveVars(qstar, simulation)
     return qstar + dt * simulation.source(qstar, primstar, auxstar, cp=0.1, eta=1/simulation.model.sig)
+
+
+def SSP2(simulation, q):
+    """
+    (S)trong-(S)tability-(P)reserving (2,2,2) IMEX scheme.
+    Coefficients taken from Pareschi & Russo '04
+    Parameters
+    ----------
+    simulation: object
+        The class containing all the data regarding the simulation
+    q : array of floats
+        The value of the field at the centre of each corresponding cell
+    Returns
+    -------
+    q : array of floats
+        The new values of the field
+    """
+    temp = simulation
+    dt = simulation.deltaT
+    gamma = 1 - 1 / np.sqrt(2)
+    psi = simulation.source
+    Nx, Ny = q[0].shape
+    q1 = np.zeros_like(q)
+    def residualQ1(guess, i, j):
+        # Determine primitive variables as a result of the guess
+        guessPrim, guessAux, guessAlpha = simulation.model.getPrimitiveVarsSingleCell(guess, simulation, i, j)
+        temp.alpha = guessAlpha
+
+        resid = guess - q[:, i, j].reshape(guess.shape) - dt * gamma \
+            * psi(guess.reshape(q[:, i, j].shape), guessPrim, guessAux, cp=0.1, eta=1/simulation.model.sig).reshape(guess.shape)
+        if np.any(np.isnan(resid)):
+            return 1e6 * np.ones_like(guess)
+        else:
+            return resid
+
+    for i in range(Nx):
+        for j in range(Ny):
+            q1[:, i, j] = fsolve(residualQ1, q[:, i, j], args=(i, j))
+
+    guessPrim, guessAux, guessAlpha = simulation.model.getPrimitiveVars(q1, simulation)
+    temp.prims = guessPrim
+    temp.aux = guessAux
+    q1 = simulation.bcs(q1, simulation.cells)
+    k1 = -simulation.F(q1, temp)
+    psi1 = psi(q1, guessPrim, guessAux, cp=0.1, eta=1/simulation.model.sig)
+
+    q2 = np.zeros_like(q1)
+    def residualQ2(guess, i, j):
+        # Determine primitive variables as a result of the guess
+        if np.any(np.isnan(guess)):
+            return 1e6 * np.ones_like(guess)
+        guessPrim, guessAux, guessAlpha = simulation.model.getPrimitiveVarsSingleCell(guess, simulation, i, j)
+        temp.prims[:, i, j] = guessPrim.reshape(temp.prims[:, i, j].shape)
+        temp.aux[:, i, j] = guessAux.reshape(temp.aux[:, i, j].shape)
+        temp.alpha = guessAlpha
+        psiguess = psi(guess.reshape(q[:, i, j].shape), guessPrim, guessAux, cp=0.1, eta=1/simulation.model.sig).reshape(guess.shape)
+        resid = guess - q[:, i, j].reshape(guess.shape) - dt * (k1[:, i, j].reshape(guess.shape) + \
+                (1 - 2 * gamma) * psi1[:, i, j].reshape(guess.shape) + gamma * \
+                psiguess)
+        if np.any(np.isnan(resid)):
+            return 1e6 * np.ones_like(guess)
+        else:
+            return resid
+
+
+    for i in range(Nx):
+        for j in range(Ny):    
+            q2[:, i, j] = fsolve(residualQ2, q1[:, i, j], args=(i, j))
+
+    guessPrim, guessAux, guessAlpha = simulation.model.getPrimitiveVars(q2, simulation)
+    temp.prims = guessPrim
+    temp.aux = guessAux
+
+
+    q2 = simulation.bcs(q2, simulation.cells)
+    k2 = -simulation.F(q2, temp)
+    psi2 = psi(q2, guessPrim, guessAux, cp=0.1, eta=1/simulation.model.sig)
+
+    # Quick check that variables make sense
+    assert(not np.all(np.isnan(simulation.aux[1]))), "Time integrator returns NaN"
+
+    return q + 0.5 * dt * (k1 + k2 + psi1 + psi2)
